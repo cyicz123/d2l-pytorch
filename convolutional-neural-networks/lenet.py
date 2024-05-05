b63b82c7000ebda7a0c5cec15aeb64ea5f87f81c
cyicz123 <cyicz123@outlook.com>
2024-05-05 17:01:32 +0800

    feat(data_loader): implement a data loader for FashionMNIST
    Signed-off-by: cyicz123 <cyicz123@outlook.com>

--- convolutional-neural-networks/lenet.py
+++ convolutional-neural-networks/lenet.py
@@ -1,5 +1,18 @@
 import torch
 from torch import nn
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def load_data_fashion_mnist(batch_size=256):
+    trans = transforms.ToTensor()
+
+    mnist_train = datasets.FashionMNIST(root="../data", train=True, download=True, transform=trans)
+    mnist_test = datasets.FashionMNIST(root="../data", train=False, download=True, transform=trans)
+
+    train_iter = DataLoader(mnist_train, batch_size=batch_size, shuffle=True)
+    test_iter = DataLoader(mnist_test, batch_size=batch_size, shuffle=False)
+
+    return train_iter, test_iter
 
 net = nn.Sequential(
     nn.Conv2d(1, 6, kernel_size=5, padding=2), nn.Sigmoid(),
@@ -11,3 +24,7 @@ net = nn.Sequential(
     nn.Linear(120, 84), nn.Sigmoid(),
     nn.Linear(84, 10)
 )
+
+batch_size = 256
+train_iter, test_iter = load_data_fashion_mnist(batch_size=batch_size)
+
